fd77e82e1394a93100c48dc5642da48fdd6acef0
jani8980 <jani.bavesh95@gmail.com>
2024-08-09 10:30:36 +0100

    Done

---
+++ src/main/java/com/techpanda/live/Github.java
@@ -0,0 +1,8 @@
+package com.techpanda.live;
+
+public class Github {
+    public static void main(String[] args) {
+        System.out.println("Print LN");
+        System.out.println("Bhavesh");
+    }
+}
